91e2af4790192e20e0035bf7047833ab510ad83a
Brad-Davidson <davidsbw@mail.uc.edu>
2021-01-30 19:58:19 -0500

    Script for Lab 3

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,6 +1,14 @@
-﻿function getIP{
- (Get-NetIPAddress).IPv4Address | Select-String "192*"
- }
- $IP = getIP
- Write-Host("This machine's IP is $IP")
- Write-Host("This machine's IP is {0}" -f $IP)
+﻿$ipv4= (Get-NetIPAddress).IPv4Address | Select-String "192*" #The IP address of the machine
+$user= $env:USERNAME #Gets the user name of the current logged in user
+$psversion= $host.version.Major
+$myhost= hostname
+$date= (Get-Date)
+$body= @"
+This machine's IP is $ipv4
+User is $user
+Hostname is $myhost
+Version $psversion
+Today's Date is $date
+"@
+Write-Host($body)
+Send-MailMessage -To "davidsbw@mail.uc.edu" -From "davidsbw@mail.uc.edu" -Subject "IT3038C Windows SysInfo" -Body "$body" -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
