cd7b736642d642bcb16fc8a17bb83f4da91ef59c
Brad-Davidson <davidsbw@mail.uc.edu>
2021-02-07 15:14:59 -0500

    added myfirstscript.sh

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,6 @@
+greeting="This is a script. Hello!"
+echo $greeting, thanks for joining us!
+echo '$greeting, thanks for joining us! You owe me $20'
+echo "$greeting, thanks for joining us!"
+echo "$greeting, you ow me $20"
+
