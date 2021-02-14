6dae1a2fbab5da650c5d72d633dc30bc3f2b2850
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-02-14 09:25:55 -0500

    Updated lab5.py to project code

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,10 +1,14 @@
+import datetime
 
-import socket, sys
+#Assignment: Lab 5
+#Name: Bradley Davidson
+#Date: 2/14/2021
 
+#prompt: Take a birthday date input and calculate how many seconds old you are 
 
-try:
-  hostname = str(sys.argv[1])
-  ip = socket.gethostbyname(hostname)
-  print (hostname +' has an IP of ' + ip)
-except:
-  print("Oops, something is wrong with that host")
+birthday = input('When is your birthday? (mm/dd/yyyy) ')
+month, day, year = map(int, birthday.split('/')) #parses the string input into a usable format for timespan
+birthday_date = datetime.datetime(year, month, day, 0) #creates a datetime object with time set to midnight
+today_date = datetime.datetime.now() #gets current date and time
+timespan = today_date - birthday_date #creates a time delta object
+print("You have been alive for " + str(int(timespan.total_seconds())) + " seconds, wow!") # for our purposes, I don't really care about micro seconds. I casted to int to easily chop off decimal point
